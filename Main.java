45987c818d48cd229ddcd479ec3aabe2506c1d86
Job Daniel Correa <jobcorre0912@gmail.com>
2026-06-02 13:41:00 -0500

    Prueba de objetos y sustiticiones en Figura y derivadas

--- Main.java
+++ Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        
+        // LSP: referencias de Figura sustituidas por Circulo y Rectangulo
+        Figura circulo = new Circulo(5);
+        Figura rectangulo = new Rectangulo(8, 4);
+
+        circulo.mostrarArea();
+        rectangulo.mostrarArea();
+
+        // Demostrando sustitucion sin alterar el programa
+        System.out.println("\n--- Sustitucion LSP ---");
+        Figura[] figuras = {new Circulo(3), new Rectangulo(6, 2)};
+        for (Figura f : figuras) {
+            f.mostrarArea();
+        }
     }
 }
